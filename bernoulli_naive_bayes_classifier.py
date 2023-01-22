2f71f176cd7a3dedd5651e351f9856fc603967da
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-01-22 13:48:51 +0800

    finish bernoulli naive classifier

--- bernoulli_naive_bayes_classifier.py
+++ bernoulli_naive_bayes_classifier.py
@@ -1,12 +1,53 @@
 import numpy as np
 
 class BernoulliNB():
-    def __init__(self, alpha=1.0):
-        print("Call init")
+    def __init__(self, alpha=1.0, beta=1.0):
         self.alpha = alpha
+        self.beta = beta
 
     def __call__(self, X):
-        print("Call predict")
+        result = dict()
+        for class_name, _ in self.cls_number.items():
+            result[class_name] = self.prior[class_name]
+            # improve later
+            for i in range(len(X[0])):
+                if X[0][i] == 1:
+                    result[class_name] *= self.params[class_name][i]
+                else:
+                    result[class_name] *= (1 - self.params[class_name][i])
+            i += 1
+        total = 0
+
+        for _, value in result.items():
+            total += value
+        for key, value in result.items():
+            result[key] = value / total
+
+        return result
 
     def fit(self, X, y):
-        print("Call fit the model")
+        # identify classes
+        classes = np.unique(y)
+        self.cls_number = dict()
+        
+        i = 0
+        for class_name in classes:
+            self.cls_number[class_name] = i
+            i = i + 1
+
+        # count each class
+        self.doc_in_class = dict()
+        self.prior = dict()
+        self.params = dict()
+        for class_name, _ in self.cls_number.items():
+            self.doc_in_class[class_name] = np.count_nonzero(y == class_name)
+            self.prior[class_name] = self.doc_in_class[class_name] / len(y)
+            for value, label in zip(X, y):
+                if label == class_name:
+                    if label not in self.params.keys():
+                        self.params[label] = value
+                    else:
+                        self.params[label] = self.params[label] + value
+                    self.params[label] = (self.params[label] + self.alpha) / (np.count_nonzero(y == class_name) + self.alpha + self.beta)
+        
+        return self
---
+++ test.py
@@ -0,0 +1,22 @@
+from bernoulli_naive_bayes_classifier import BernoulliNB
+import numpy as np
+
+# train data
+d1 = [1, 1, 1, 0, 0, 0, 0, 0, 0]
+d2 = [1, 1, 0, 1, 1, 0, 0, 0, 0]
+d3 = [0, 1, 0, 0, 1, 1, 0, 0, 0]
+d4 = [0, 1, 0, 0, 0, 0, 1, 1, 1]
+
+train_data = np.array([d1, d2, d3, d4])
+label = np.array(['B', 'B', 'B', 'N']) # 0 - B, 1 - N 
+
+# test data
+d5 = np.array([[1, 0, 0, 1, 0, 0, 0, 1, 0]])
+d6 = np.array([[0, 1, 0, 0, 0, 0, 0, 1, 1]])
+
+clf = BernoulliNB()
+clf.fit(train_data, label)
+# clf(d1)
+
+print(clf(d5))
+print(clf(d6))
